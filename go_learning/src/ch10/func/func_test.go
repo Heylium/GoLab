23e65506398f478c71700b84c6aa5493b71d7c23
Heylium <tuanhsiang@163.com>
2024-06-07 00:12:41 +0800

    ch10 commit

--- go_learning/src/ch10/func/func_test.go
+++ go_learning/src/ch10/func/func_test.go
@@ -1,1 +1,35 @@
-package _func
+package fn_test
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func returnMultiValues() (int, int) {
+	return rand.Intn(10), rand.Intn(20)
+}
+
+func timeSpent(inner func(op int) int) func(op int) int {
+	return func(n int) int {
+		start := time.Now()
+		ret := inner(n)
+
+		fmt.Printf("time spent: %v \n", time.Since(start).Seconds())
+		return ret
+	}
+}
+
+func slowFunc(op int) int {
+	time.Sleep(time.Second * 1)
+	return op
+}
+
+func TestFn(t *testing.T) {
+	a, b := returnMultiValues()
+	t.Log(a, b)
+
+	tsSf := timeSpent(slowFunc)
+	t.Log(tsSf(10))
+}
