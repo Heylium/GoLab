7ee7eaf153efde255590e0d8c8f15451ec766f30
Heylium <tuanhsiang@163.com>
2024-06-11 00:55:30 +0800

    ch11 commit2

---
+++ go_learning/src/ch11/custom_type/customer_type_test.go
@@ -0,0 +1,30 @@
+package customer_type
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+type IntConv func(op int) int
+
+func timeSpent(inner IntConv) IntConv {
+	return func(n int) int {
+		start := time.Now()
+		ret := inner(n)
+
+		fmt.Printf("time spent: %v \n", time.Since(start).Seconds())
+		return ret
+	}
+}
+
+func slowFunc(op int) int {
+	time.Sleep(time.Second * 1)
+	return op
+}
+
+func TestFn(t *testing.T) {
+
+	tsSf := timeSpent(slowFunc)
+	t.Log(tsSf(10))
+}
