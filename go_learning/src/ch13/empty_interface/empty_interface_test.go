30c6b9480462e5fdaf532e6973f9e5c30c542f7b
Heylium <tuanhsiang@163.com>
2024-06-12 00:58:59 +0800

    ch13 commit

--- go_learning/src/ch13/empty_interface/empty_interface_test.go
+++ go_learning/src/ch13/empty_interface/empty_interface_test.go
@@ -1,1 +1,37 @@
 package empty_interface
+
+import (
+	"fmt"
+	"testing"
+)
+
+func DoSomething(p interface{}) {
+	if i, ok := p.(int); ok {
+		fmt.Println("Integer", i)
+		return
+	}
+	if s, ok := p.(string); ok {
+		fmt.Println("String", s)
+		return
+	}
+	fmt.Println("Unknown Type")
+}
+
+func TypeAssertion2(p interface{}) {
+	switch t := p.(type) {
+	case int:
+		fmt.Println("Integer", t)
+	case string:
+		fmt.Println("String", t)
+	default:
+		fmt.Println("Unknown Type")
+	}
+}
+
+func TestEmptyInterfaceAssertion(t *testing.T) {
+	DoSomething(10)
+	DoSomething("10")
+
+	TypeAssertion2(10)
+	TypeAssertion2("10")
+}
