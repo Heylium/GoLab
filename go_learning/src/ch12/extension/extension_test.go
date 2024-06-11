86ceb9fb219261c79300c39438566bfd553379ff
Heylium <tuanhsiang@163.com>
2024-06-12 00:57:27 +0800

    ch12 commit2

--- go_learning/src/ch12/extension/extension_test.go
+++ go_learning/src/ch12/extension/extension_test.go
@@ -1,1 +1,37 @@
 package extension
+
+import (
+	"fmt"
+	"testing"
+)
+
+type Pet struct {
+}
+
+func (p *Pet) Speak() {
+	fmt.Println("I am a pet")
+}
+
+func (p *Pet) SpeakTo(host string) {
+	p.Speak()
+	fmt.Println(" ", host)
+}
+
+type Dog struct {
+	Pet
+}
+
+func (d *Dog) Speak() {
+	fmt.Println("Wang!")
+}
+
+func (d *Dog) SpeakTo(host string) {
+	d.Speak()
+	fmt.Println(" ", host)
+}
+
+func TestDog(t *testing.T) {
+	dog := new(Dog)
+	dog.Speak()
+	dog.SpeakTo("Chao")
+}
