d5bfe2ae66515571b01ab515063f7c81a5ff98b3
Heylium <tuanhsiang@163.com>
2024-06-15 01:42:34 +0800

    ch18 commit

---
+++ go_learning/src/ch18/csp/async_service_test.go
@@ -0,0 +1,41 @@
+package csp
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func service() string {
+	time.Sleep(50 * time.Millisecond)
+	return "Done"
+}
+
+func otherTask() {
+	fmt.Println("Other task")
+	time.Sleep(50 * time.Millisecond)
+	fmt.Println("Other task done")
+}
+
+func TestService(t *testing.T) {
+	fmt.Println(service())
+	otherTask()
+}
+
+func AsyncService() chan string {
+	// add buffer to
+	retCh := make(chan string, 1)
+	go func() {
+		ret := service()
+		fmt.Println("returned result.")
+		retCh <- ret
+		fmt.Println("service exited.")
+	}()
+	return retCh
+}
+
+func TestAsyncService(t *testing.T) {
+	retCh := AsyncService()
+	otherTask()
+	fmt.Println(<-retCh)
+}
