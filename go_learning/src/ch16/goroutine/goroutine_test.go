e2a2de8a8170251a7e5429799ed0ac6a0251c096
Heylium <tuanhsiang@163.com>
2024-06-15 01:20:22 +0800

    ch16 commit

--- go_learning/src/ch16/goroutine/goroutine_test.go
+++ go_learning/src/ch16/goroutine/goroutine_test.go
@@ -1,1 +1,21 @@
-package goroutine
+package goroutine_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestGoroutine(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		// notice! do not write below in older go earlier than 1.22
+		//go func() {
+		//	fmt.Println(i)
+		//}()
+
+		go func(i int) {
+			fmt.Printf("goroutine %d\n", i)
+		}(i)
+	}
+	time.Sleep(time.Millisecond * 50)
+}
