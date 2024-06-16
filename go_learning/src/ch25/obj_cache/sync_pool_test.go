3075612d83af5a716b08a5dcd119ae08158a3359
Heylium <tuanhsiang@163.com>
2024-06-16 17:30:53 +0800

    ch25 commit2

--- go_learning/src/ch25/obj_cache/sync_pool_test.go
+++ go_learning/src/ch25/obj_cache/sync_pool_test.go
@@ -1,1 +1,46 @@
 package obj_cache
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+func TestSyncPool(t *testing.T) {
+	pool := &sync.Pool{
+		New: func() interface{} {
+			fmt.Println("Create a new object")
+			return 100
+		},
+	}
+
+	v := pool.Get().(int)
+	fmt.Println(v)
+	pool.Put(200)
+	//runtime.GC()
+	v1, _ := pool.Get().(int)
+	fmt.Println(v1)
+}
+
+func TestSyncPoolInMultiGoroutine(t *testing.T) {
+	pool := &sync.Pool{
+		New: func() interface{} {
+			fmt.Println("Create a new object")
+			return 10
+		},
+	}
+
+	pool.Put(200)
+	pool.Put(200)
+	pool.Put(200)
+
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(i int) {
+			fmt.Println(pool.Get())
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+}
