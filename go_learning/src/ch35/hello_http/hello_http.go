54359976981715d75772045335ee202768adedc5
Heylium <tuanhsiang@163.com>
2024-06-19 00:56:10 +0800

    ch35 commit

--- go_learning/src/ch35/hello_http/hello_http.go
+++ go_learning/src/ch35/hello_http/hello_http.go
@@ -10,7 +10,7 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World")
 	})
-	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/time/", func(w http.ResponseWriter, r *http.Request) {
 		t := time.Now()
 		timeStr := fmt.Sprintf("{\"time\": \"%s\"}", t)
 		w.Write([]byte(timeStr))
